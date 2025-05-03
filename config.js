805cb9f644ec8bed254f2544a252eb5d4fbd5e5f
outlawstar4761 <outlawstar4761@gmail.com>
2025-05-02 22:10:07 -0500

    fixing default assignment

--- config.js
+++ config.js
@@ -5,22 +5,22 @@ process.env.MYSQL_USER = process.env.MYSQL_USER || 'root';
 process.env.MYSQL_PASS = process.env.MYSQL_PASS || 'example';
 process.env.MYSQL_CRON_DB = process.env.MYSQL_CRON_DB || 'cron_test';
 //allows for parameterless init of authClient in ./src/cronServer
-process.env.OD_ACCOUNTS_BASE_URL = 'https://localhost:9661';
+process.env.OD_ACCOUNTS_BASE_URL = process.env.OD_ACCOUNTS_BASE_URL || 'https://localhost:9661';
 
 export default {
   development:{
     PORT:9550,
-    SSLCERTPATH: '/etc/apache2/certs/fullchain.pem',
-    SSLKEYPATH: '/etc/apache2/certs/privkey.pem',
+    SSLCERTPATH:'/etc/apache2/certs/fullchain.pem',
+    SSLKEYPATH:'/etc/apache2/certs/privkey.pem',
   },
   testing:{
     PORT:9550,
-    SSLCERTPATH: '/etc/apache2/certs/fullchain.pem',
-    SSLKEYPATH: '/etc/apache2/certs/privkey.pem',
+    SSLCERTPATH:'/etc/apache2/certs/fullchain.pem',
+    SSLKEYPATH:'/etc/apache2/certs/privkey.pem',
   },
   production:{
     PORT:9550,
-    SSLCERTPATH: '/etc/apache2/certs/fullchain.pem',
-    SSLKEYPATH: '/etc/apache2/certs/privkey.pem',
+    SSLCERTPATH:'/etc/apache2/certs/fullchain.pem',
+    SSLKEYPATH:'/etc/apache2/certs/privkey.pem',
   }
 };
