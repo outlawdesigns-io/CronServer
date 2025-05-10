f08ef86423de50e4996222781bb1efa8a050202a
outlawstar4761 <outlawstar4761@gmail.com>
2025-05-09 22:30:49 -0500

    adding proxy header support

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ global.config = config;
 
 /*SETUP THE EXPRESS SERVER*/
 const app = express();
+app.set('trust proxy',true);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth_token");
